6ec727496427a08840e107773d5a919ad4dc17eb
Nick Torres <nickrtorres@icloud.com>
2020-04-12 18:20:21 -0700

    trees: add formatting for printing tree in order

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@
 //! A *complete* implementation of the `BinaryTree` sample given in
 //! *Programming Rust* by Jim Blandy and Jason Orendorff.
 use std::cmp::{Ord, Ordering};
+use std::fmt::{self, Formatter};
 use std::iter::FromIterator;
-
 /// A binary has two variants: Empty and `NonEmpty`. A `NonEmpty` variant
 /// represents a node on the tree that has pointers to two other `BinaryTree`
 /// nodes. An `Empty` variant represents a leaf.
@@ -13,10 +13,37 @@ pub enum BinaryTree<T> {
     NonEmpty(Box<TreeNode<T>>),
 }
 
-impl<T: Ord> BinaryTree<T> {
+impl<T: Ord + fmt::Debug> fmt::Display for BinaryTree<T> {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
+        return self.write_in_order(f);
+    }
+}
+
+impl<T: Ord + fmt::Debug> BinaryTree<T> {
     // TODO: use Default?
     pub fn new() -> Self {
         BinaryTree::Empty
+    }
+
+    fn write_in_order(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
+        match self.as_option() {
+            None => {}
+            Some(t) => {
+                t.left.write_in_order(f)?;
+                write!(f, "{:?}", t.element)?;
+                t.right.write_in_order(f)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    /// adapter into an Option<&TreeNode>
+    fn as_option(&self) -> Option<&TreeNode<T>> {
+        match *self {
+            Self::Empty => None,
+            Self::NonEmpty(ref b) => Some(b),
+        }
     }
 
     /// Gets the number of elements from the current element down
@@ -50,8 +77,8 @@ impl<T: Ord> BinaryTree<T> {
                 }));
             }
             Self::NonEmpty(ref mut b) => match Ord::cmp(&b.element, &value) {
-                Ordering::Less => b.left.add(value),
-                Ordering::Greater => b.right.add(value),
+                Ordering::Less => b.right.add(value),
+                Ordering::Greater => b.left.add(value),
                 Ordering::Equal => unimplemented!(),
             },
         }
@@ -68,12 +95,34 @@ impl<T: Ord> BinaryTree<T> {
         match *self {
             Self::Empty => None,
             Self::NonEmpty(ref b) => match Ord::cmp(&b.element, value) {
-                Ordering::Less => b.left.get(value),
-                Ordering::Greater => b.right.get(value),
+                Ordering::Less => b.right.get(value),
+                Ordering::Greater => b.left.get(value),
                 Ordering::Equal => Some(&b.element),
             },
         }
     }
+
+    /// Returns the smallest (leftmost) element in the tree
+    pub fn min(&self) -> Option<&T> {
+        match *self {
+            Self::Empty => None,
+            Self::NonEmpty(ref b) => match b.left {
+                Self::Empty => Some(&b.element),
+                Self::NonEmpty(ref l) => l.left.min(),
+            },
+        }
+    }
+
+    pub fn max(&self) -> Option<&T> {
+        match *self {
+            Self::Empty => None,
+            Self::NonEmpty(ref b) => match b.right {
+                Self::Empty => Some(&b.element),
+                Self::NonEmpty(ref l) => l.right.min(),
+            },
+        }
+    }
+}
 }
 
 impl<T: Ord> FromIterator<T> for BinaryTree<T> {
@@ -157,4 +206,38 @@ mod tests {
         let tree: BinaryTree<u32> = vec![1, 2, 3, 4].iter().cloned().collect();
         assert_eq!(4, tree.size());
     }
+
+    #[test]
+    fn it_can_find_the_min() {
+        type Tree = BinaryTree<u32>;
+        let mut tree = Tree::new();
+
+        tree.add(7);
+        tree.add(6);
+        tree.add(5);
+        tree.add(4);
+        tree.add(3);
+        tree.add(2);
+        tree.add(1);
+
+        assert_eq!(Some(&1), tree.min());
+    }
+
+    #[test]
+    fn it_can_find_the_max() {
+        type Tree = BinaryTree<u32>;
+        let mut tree = Tree::new();
+
+        tree.add(8);
+        tree.add(1);
+        tree.add(2);
+        tree.add(3);
+        tree.add(4);
+        tree.add(5);
+        tree.add(6);
+        tree.add(7);
+
+        assert_eq!(Some(&8), tree.max());
+    }
+
 }
